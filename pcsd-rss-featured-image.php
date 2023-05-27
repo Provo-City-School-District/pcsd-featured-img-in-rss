09a6e786e135df221e78fe5c02ebca6ce743b48d
Espi <joshespi@gmail.com>
2023-05-27 09:05:44 -0600

    Update pcsd-rss-featured-image.php
    changes I made:
    
    Renamed the function pcsdfeaturedtoRSS to pcsd_featured_to_rss to follow the standard PHP naming convention.
    Removed the unnecessary $buildingImage variable declaration at the beginning.
    Simplified the logic for retrieving the featured image URL and storing it in $image_src.
    Updated the image source assignment to concatenate the image URL with the HTML string.
    Cleaned up the indentation and formatting for better readability.
    Removed the unused plugin information comments for brevity.

--- pcsd-rss-featured-image.php
+++ pcsd-rss-featured-image.php
@@ -1,38 +1,33 @@
 <?php
-
 /* 
-Plugin Name: PCSD add featured image to RSS feed
-Description: Add Feature Image to your RSS Feed. works with pcsd-featured-area
+Plugin Name: PCSD Add Featured Image to RSS Feed
+Description: Add Featured Image to your RSS Feed. Works with pcsd-featured-area.
 Author: Josh Espinoza
 Version: 1.01
-
-
-ripped pieces from http://dineshkarki.com.np/featured-image-in-rss-feed when wordfence started marking it as a critical error.
 */
-$buildingImage = WP_CONTENT_URL . '/themes/pcsdtwentytwentythree/assets/images/building-image.jpg';
-function pcsdfeaturedtoRSS($content)
-{
+
+function pcsd_featured_to_rss($content) {
     global $post;
-    if (get_field('featured_image', $post)) {
-?>
-        <img src="<?php echo get_field('featured_image'); ?>" />
-    <?php
+
+    $featured_image = get_field('featured_image', $post);
+    
+    if ($featured_image) {
+        $image_src = $featured_image;
     } elseif (has_post_thumbnail()) {
-    ?>
-        <img src="<?php echo get_the_post_thumbnail_url(); ?>" />
-    <?php
-    } elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . $buildingImage)) {
-        $buildingImage = get_stylesheet_directory_uri() . '/assets/images/building-image.jpg';
-    ?>
-        <img src="<?php echo $buildingImage; ?>" />
-    <?php
+        $image_src = get_the_post_thumbnail_url();
     } else {
-    ?>
-        <img src="https://provo.edu/wp-content/uploads/2018/03/provo-school-district-logo.jpg" />
-<?php
+        $building_image = WP_CONTENT_URL . '/themes/pcsdtwentytwentythree/assets/images/building-image.jpg';
+        
+        if (file_exists($_SERVER['DOCUMENT_ROOT'] . $building_image)) {
+            $image_src = get_stylesheet_directory_uri() . '/assets/images/building-image.jpg';
+        } else {
+            $image_src = 'https://provo.edu/wp-content/uploads/2018/03/provo-school-district-logo.jpg';
+        }
     }
+
+    $content = '<img src="' . $image_src . '" />' . $content;
     return $content;
 }
 
-add_filter('the_excerpt_rss', 'pcsdfeaturedtoRSS');
-add_filter('the_content_feed', 'pcsdfeaturedtoRSS');
+add_filter('the_excerpt_rss', 'pcsd_featured_to_rss');
+add_filter('the_content_feed', 'pcsd_featured_to_rss');
